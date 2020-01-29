9852fc895dcfb3eed652262d8b01eb685d05b11d
Hannah VanWingen <hvanova@umich.edu>
2020-01-29 17:00:42 -0500

    in progress

--- plugin/components/SmMore.vue
+++ plugin/components/SmMore.vue
@@ -1,82 +1,99 @@
 <template>
-  <div class="relative" :class="{ raiseZ: expanded }" @focusout="expanded=false">
-    <SmButton
-      icon="more-vertical"
-      class="icon"
+  <div class="SmMore" :class="{ raiseZ: expanded }" @focusout="expanded=false">
+    <div
+      class="icon right-0"
+      kind="icon"
       :class="{ iconBorder: expanded }"
       @click="expanded = !expanded"
-    />
+    >
+      <SmIcon name="more-vertical" size="s" />
+    </div>
     <div v-if="expanded" class="options">
       <div
         v-for="(option, index) in options"
         :key="index"
-        class="option flex items-center"
+        class="option pt-1 pb-1"
         @click="
           $emit(option.replace(' ', '_').toLowerCase()), (expanded = false)
         "
       >
-        <div class="pl-2 pr-10">{{ option }}</div>
+        <div class="pl-2 pr-2">{{ option }}</div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import SmIcon from "./SmIcon.vue";
+
 export default {
   name: "SmMore",
+  components: {
+    SmIcon
+  },
+  props: {
+    /** A list of options to display on dropdown. */
+    options: Array,
+    required: true
+  },
   data: function() {
     return {
       expanded: false
     };
-  },
-  props: {
-    options: Array
   }
 };
 </script>
 
 <style scoped>
 .icon {
-  border-radius: 2px;
-  cursor: pointer;
   background: white;
-  float: right;
   border: 1px solid white;
-  border-bottom: 0px solid white;
+  border-bottom: 0px;
   width: 32px;
   height: 32px;
-  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  position: absolute;
 }
+
 .iconBorder {
-  z-index: 60;
   border-radius: 2px 2px 0px 0px;
   border-top: 1px solid black;
   border-right: 1px solid black;
   border-left: 1px solid black;
+  z-index: 9999;
 }
+
 .options {
   border-radius: 2px 0px 2px 2px;
   border: 1px solid black;
   background: white;
-  transform: translateY(31px);
   position: absolute;
   right: 0;
+  min-width: 80px;
+  transform: translateY(31px);
 }
 .raiseZ {
-  z-index: 50;
+  z-index: 9998;
 }
 .option {
   border-radius: 0px;
-  height: 32px;
+  min-height: 32px;
   cursor: pointer;
-  width: 100%;
 }
-.option:focus,
-.icon:focus {
-  outline-offset: -2px;
-}
+
 .option:hover {
   background: var(--primary-hover);
   color: white;
+}
+
+.SmMore {
+  position: relative;
+  margin-left: auto;
+  border: 1px solid green;
+  min-width: 80px;
+  width: 100%;
 }
 </style>

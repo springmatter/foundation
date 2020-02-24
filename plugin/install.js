4577baf6f4ab6aaf0d5b7b4de18efc2d1e587fcf
mdkq <marcusdk@icloud.com>
2020-02-24 10:19:38 -0500

    cleaning up

--- plugin/install.js
+++ plugin/install.js
@@ -6,30 +6,26 @@ import "tachyons";
 
 import VueRouter from "vue-router";
 
-import SmApp from "./components/SmApp.vue";
 import SmButton from "./components/SmButton.vue";
-import SmCard from "./components/SmCard.vue";
 import SmIcon from "./components/SmIcon.vue";
 import SmImage from "./components/SmImage.vue";
-import SmSpinner from "./components/SmSpinner.vue";
-import SmMore from "./components/SmMore.vue";
-import SmScrollBox from "./components/SmScrollBox.vue";
+import SmInput from "./components/SmInput.vue";
+import SmModal from "./components/SmModal.vue";
+import SmNav from "./components/SmNav.vue";
 import SmSearch from "./components/SmSearch.vue";
-import SmTag from "./components/SmTag.vue";
 import SmSelect from "./components/SmSelect.vue";
+import SmSpinner from "./components/SmSpinner.vue";
 
 export default {
   install(Vue) {
-    Vue.component("SmApp", SmApp);
     Vue.component("SmButton", SmButton);
-    Vue.component("SmCard", SmCard);
     Vue.component("SmIcon", SmIcon);
     Vue.component("SmImage", SmImage);
+    Vue.component("SmInput", SmInput);
+    Vue.component("SmModal", SmModal);
+    Vue.component("SmNav", SmNav);
     Vue.component("SmSpinner", SmSpinner);
-    Vue.component("SmMore", SmMore);
-    Vue.component("SmScrollBox", SmScrollBox);
     Vue.component("SmSearch", SmSearch);
-    Vue.component("SmTag", SmTag);
     Vue.component("SmSelect", SmSelect);
 
     Vue.use(VueRouter);
